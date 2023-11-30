ca22bff6f974745f4e351868579aac8877b5b4a5
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-11-30 17:18:09 +0400

    fix: Fixes install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,4 @@
 sudo -v
 curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/kaytu-io/cli/releases/latest | jq -r .assets[].browser_download_url | grep linux | grep amd | xargs wget -O ./kaytu -qi -
 chmod +x ./kaytu
-mv ./kaytu /usr/bin/
+sudo mv ./kaytu /usr/bin/
